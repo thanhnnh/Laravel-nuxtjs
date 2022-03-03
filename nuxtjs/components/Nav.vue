fbde9a2cd9bf0535d4699dd2005b437e8d048a8a
ThanhDC <duongcongthanh97@gmail.com>
2022-03-03 16:51:23 +0700

    laravel and nuxtjs

---
+++ nuxtjs/components/Nav.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="bg-teal-500 text-white text-md">
+        <nav class="container flex justify-between items-center max-w-6xl mx-auto py-5 sm:px-6 lg:px-8">
+            <span class="font-semibold text-2xl">Friendzy</span>
+            <div class="space-x-2">
+                <template v-if="this.$auth.loggedIn">
+                    <span>Welcome,
+                        <NuxtLink to="#" class="capitalize hover:text-teal-200">
+                            {{ this.$auth.user.username }}
+                        </NuxtLink>
+                    </span>
+                        <NuxtLink to="/users" class="hover:text-teal-200">
+                            USERS
+                        </NuxtLink>
+                        <NuxtLink to="/dashboard" class="hover:text-teal-200">
+                            DASHBOARD
+                        </NuxtLink>
+                        <a href="#" @click.prevent="logout" class="hover:text-teal-200">LOGOUT</a>
+                </template>
+                <template v-else>
+                    <NuxtLink to="/auth/register" class="hover:text-teal-200">
+                        REGISTER
+                    </NuxtLink>
+                    <NuxtLink to="/auth/login" class="hover:text-teal-200">
+                        LOGIN
+                    </NuxtLink>
+                </template>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            logout() {
+                this.$auth.logout()
+            }
+        }
+    }
+</script>
---
+++ nuxtjs/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container max-w-6xl mx-auto mt-48 sm:px-6 lg:px-8">
+      <div class="flex justify-center items-center bg-teal-200 h-56">
+          <span class="text-gray-700 text-5xl">TestNuxt js</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    middleware: 'guest',
+    head: {
+        title: '',
+    },
+}
+</script>
